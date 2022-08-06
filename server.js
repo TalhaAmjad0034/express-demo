939a9486c770691db1d9e55ffd070fe3ecf4d1b8
Talha <73413298+TalhaAmjad0034@users.noreply.github.com>
2022-08-06 20:45:56 +0500

    added logic for adding new friend

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    console.log(req.body);
+    return res.status(400).send("Name is required");
+  }
+
+  const newFriend = {
+    id: friends.length,
+    name: req.body.name,
+  };
+  friends.push(newFriend);
+  res.json(newFriend);
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
